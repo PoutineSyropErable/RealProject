79adb9a1298e0388d00c9ca631a4f4de8fcbe248
François Pagé <francois.page@mail.mcgill.ca>
2025-01-04 00:14:29 -0500

    install environment recreated

--- install_requirement.sh
+++ install_requirement.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+echo "You don't execute this, you do the command in your terminal once conda is activated"
+echo 
+echo "conda env export > environment.yml"
+exit 0
+# do the following command
+# Gave it execute permission so its colored by ls
 conda env create -f environment.yml
 
